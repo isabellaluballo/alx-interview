a29389a8e908cb453fb6ffa1b467807df144f10c
loretta dhahabu <loreydjefwa@gmail.com>
2023-02-24 11:21:21 +0300

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
-"""A function to determine the fewest number of coins needed
-   to meet a given amount total"""
+"""
+Task: Change comes from within
+Given a pile of coins of different values,
+determine the fewest number of coins needed to
+meet a given amount total
+"""
 
 
 def makeChange(coins, total):
-    """This function will take a list of coins and use
-       that to calculate how much change the total will require
     """
+    Given a pile of coins of different values, determine the fewest
+    number of coins needed to meet a given amount total.
+    Return: fewest number of coins needed to meet total
+        - If total is 0 or less, return 0
+        - If total cannot be met by any number of coins you have, return -1
+        - Coins is a list of the values of the coins in your possession
+        - The value of a coin will always be an integer greater than 0
+        - You can assume you have an infinite number of each denomination of
+        coin in the list
+    """
+    n_coins = 0
     if total <= 0:
-        return 0
-
-    else:
-        coin = sorted(coins)
-        coin.reverse()
-        counter = 0
-        for i in coin:
-            while(total >= i):
-                counter += 1
-                total -= i
-        if total == 0:
-            return counter
+        return n_coins
+    coins.sort(reverse=True)
+    while (total > 0 and coins):
+        n = int(total / coins[0])
+        total = total - (coins[0] * n)
+        n_coins = n_coins + n
+        coins.remove(coins[0])
+    if total != 0:
         return -1
+    return n_coins
